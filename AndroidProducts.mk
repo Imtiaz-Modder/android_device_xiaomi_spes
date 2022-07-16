0a84159e69a6346f57090598a52a9152a7b8799c
imtiaz-modder <saikatsaikat0197@gmail.com>
2022-07-16 16:27:49 +0200

    arrowos

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,4 @@
+
 #
 # Copyright (C) 2022 The LineageOS Project
 #
@@ -5,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_spes.mk
+    $(LOCAL_DIR)/arrow_spes.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_spes-user \
-   aosp_spes-userdebug \
-    aosp_spes-eng
+    arrow_spes-user \
+   arrow_spes-userdebug \
+    arrow_spes-eng
